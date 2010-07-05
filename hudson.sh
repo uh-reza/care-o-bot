25a4093458df32a6f3bb1d256be3aed4f8f28adf
ipa-fmw <fmw@ipa.fhg.de>
2010-07-05 18:05:25 +0200

    hudson script

--- hudson.sh
+++ hudson.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-rosdep install cob_extern cob_common cob_driver cob_simulation cob_apps -y
-rosmake cob_extern cob_common cob_driver cob_simulation cob_apps --skip-blacklist
+STACKS="
+cob_apps
+cob_common
+cob_driver
+cob_extern
+cob_simulation
+"
+
+if [ $ROS_ROOT = "/opt/ros/boxturtle/ros" ]; then
+	echo "boxturtle"
+	rosdep install $STACKS -y
+	rosmake $STACKS --skip-blacklist
+elif [ $ROS_ROOT = "/opt/ros/latest/ros" ]; then
+	echo "latest"
+	rosdep install $STACKS -y
+	rosmake $STACKS
+else 
+	echo "no ros release found"
+fi
