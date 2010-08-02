ab3f6419a79a7ecb9690a3a2beaa01bda839ff50
ipa-fmw <fmw@ipa.fhg.de>
2010-08-02 09:20:41 +0200

    added ros_experimental to hudson config file

--- hudson.sh
+++ hudson.sh
@@ -27,6 +27,15 @@ echo $RELEASE
 # installing ROS release
 sudo apt-get install ros-$RELEASE-pr2all -y
 
+# checkout ros_experimental manually
+mkdir -p ~/ros
+mkdir -p ~/ros/boxturtle
+svn co https://code.ros.org/svn/ros/stacks/ros_experimental/tags/boxturtle ~/ros/boxturtle/ros_experimental
+mkdir -p ~/ros/latest
+svn co https://code.ros.org/svn/ros/stacks/ros_experimental/trunk ~/ros/latest/ros_experimental
+mkdir -p ~/ros/cturtle
+svn co https://code.ros.org/svn/ros/stacks/ros_experimental/trunk ~/ros/cturtle/ros_experimental
+
 # checking for workspace
 if [ -n "${WORKSPACE:-x}" ]; then
 	WORKSPACE=$PWD
@@ -40,6 +49,7 @@ export ROS_ROOT=/opt/ros/$RELEASE/ros
 export PATH=${ROS_ROOT}/bin:${PATH}
 export PYTHONPATH=${ROS_ROOT}/core/roslib/src
 export ROS_PACKAGE_PATH=/opt/ros/$RELEASE/stacks
+export ROS_PACKAGE_PATH=~/ros/$RELEASE/ros_experimental:$ROS_PACKAGE_PATH
 export ROS_PACKAGE_PATH=$WORKSPACE:$ROS_PACKAGE_PATH
 export ROS_PARALLEL_JOBS=-j2
 
